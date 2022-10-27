306c8211f955e98e3b8b049b71bb25353ab4c4ad
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2022-10-27 18:40:22 +0900

    起動時に現在地表示 長押しでマーカー一つ出現

--- app/src/main/java/com/example/sotsuken/MapsActivity.java
+++ app/src/main/java/com/example/sotsuken/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.sotsuken;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -10,8 +12,11 @@ import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -20,6 +25,12 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.sotsuken.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+
+import org.jetbrains.annotations.NotNull;
 
 
 /***
@@ -28,28 +39,43 @@ import com.example.sotsuken.databinding.ActivityMapsBinding;
  * カメラ設定
  * 起動時に現在地を表示
  * 長押しすることでマーカー出現or Androidアプリ開発の教科書14章参考
+ * その後にdirectionAPI起動
  */
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener,
-        ActivityCompat.OnRequestPermissionsResultCallback {
+        ActivityCompat.OnRequestPermissionsResultCallback, GoogleMap.OnMapLongClickListener {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private static final int DEFAULT_ZOOM = 15;
     private GoogleMap mMap;
     private Marker marker;
-    private boolean permissionDenied = false;
+    private boolean locationPermissionGranted;
+    private LatLng current;
+    private Location myLocation;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Location lastKnownLocation;
+    private final LatLng defaultLocation = new LatLng(35.6809591 ,139.7673068);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        com.example.sotsuken.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //com.example.sotsuken.databinding.ActivityMapsBinding binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_maps);
+
+        Places.initialize(getApplicationContext(), getString(R.string.google_maps_key));
+        PlacesClient placesClient = Places.createClient(this);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
     /**
@@ -69,16 +95,21 @@ public class MapsActivity extends FragmentActivity
         //仮の中心座標
         //アプリ起動時に現在地があればそれでよし
         // Add a marker in Sydney and move the camera
+        /*
         LatLng tokyo = new LatLng(35.6809591, 139.7673068);
         marker = mMap.addMarker(new MarkerOptions().position(tokyo).title("Marker in Tokyo"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tokyo, 14.0f));
-
+        */
+        mMap.setMyLocationEnabled(true);
+        updateLocationUI();
+        getDeviceLocation();
         //カメラの縮尺設定
         mMap.setMinZoomPreference(12.0f);
         mMap.setMaxZoomPreference(20.0f);
 
 
         //現在地設定
+
         mMap.setOnMyLocationButtonClickListener(this);
         mMap.setOnMyLocationClickListener(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -91,22 +122,128 @@ public class MapsActivity extends FragmentActivity
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        mMap.setMyLocationEnabled(true);
         enableMyLocation();
 
 
         //長押しでマーカー追加
         //マーカーは増殖しないで最新の物のみ残す
+        //現在地のマーカーをタップした後に新しくマーカーを立てないと落ちる
+        //何故なら、LatLng型のcurrentが現在地を初期から保持していないから
+        /*
         mMap.setOnMapLongClickListener(longpushLocation -> {
             LatLng target = new LatLng(longpushLocation.latitude, longpushLocation.longitude);
+
             marker.remove();
             marker = mMap.addMarker(new MarkerOptions().position(target).title("" + longpushLocation.latitude + " :" + longpushLocation.longitude));
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(target, 14));
-            Toast.makeText(this, "target:\n" + longpushLocation, Toast.LENGTH_LONG).show();
+
+            Toast.makeText(this, "current:("+current.latitude +","+current.longitude+ ")\ntarget:(" + longpushLocation.latitude +","+ longpushLocation.longitude+")", Toast.LENGTH_LONG).show();
 
         });
 
+         */
+        mMap.setOnMapLongClickListener(this);
 
+
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (locationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+
+    private void getDeviceLocation() {
+        /*
+        if (locationPermissionGranted) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+            locationTask.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull @NotNull Task<Location> task) {
+                    if (task.isSuccessful()) {
+                        myLocation = task.getResult();
+                        if (myLocation != null) {
+                            // カメラ位置移動
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLocation.getLatitude(), myLocation.getLongitude()), 15));
+                        }
+                    }
+                }
+            });
+        }
+         */
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            mMap.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -115,6 +252,7 @@ public class MapsActivity extends FragmentActivity
                 == PackageManager.PERMISSION_GRANTED
                 || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
+            Log.d("debug", "setMyLocationEnabled(true)");
             mMap.setMyLocationEnabled(true);
             return;
         }
@@ -133,7 +271,7 @@ public class MapsActivity extends FragmentActivity
 
     @Override
     public void onMyLocationClick(@NonNull Location location) {
-        LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
+        current = new LatLng(location.getLatitude(), location.getLongitude());
         //mMap.addMarker(new MarkerOptions().position(tokyo).title("Marker in Tokyo"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current, 20.0f));
@@ -158,20 +296,24 @@ public class MapsActivity extends FragmentActivity
         } else {
             // Permission was denied. Display an error message
             // Display the missing permission error dialog when the fragments resume.
-            permissionDenied = true;
+            locationPermissionGranted = true;
         }
+        updateLocationUI();
     }
 
 
+
+    /*
     @Override
     protected void onResumeFragments() {
         super.onResumeFragments();
-        if (permissionDenied) {
+        if (locationPermissionGranted) {
             // Permission was not granted, display error dialog.
             showMissingPermissionError();
-            permissionDenied = false;
+            locationPermissionGranted = false;
         }
     }
+     */
 
     /**
      * Displays a dialog with error message explaining that the location permission is missing.
@@ -179,5 +321,19 @@ public class MapsActivity extends FragmentActivity
     private void showMissingPermissionError() {
         PermissionUtils.PermissionDeniedDialog
                 .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        Log.d("debug", String.valueOf(current));
+        if (marker != null) {
+            marker.remove();
+        }
+        marker = mMap.addMarker(new MarkerOptions().position(latLng).title("(" + latLng.latitude + "," + latLng.longitude + ")"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14));
+
+        //"current:("+current.latitude +","+current.longitude+ ")\ntarget:(" +
+        Toast.makeText(this, latLng.latitude + "," + latLng.longitude + ")", Toast.LENGTH_LONG).show();
+
     }
 }
